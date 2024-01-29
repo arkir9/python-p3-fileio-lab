9e4f4094004da9c876609d691558c36abe0e1b84
Mohammed <khamismitch@gmail.com>
2024-01-29 11:05:32 +0300

    done

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,23 @@
+# file_io.py
+
 def write_file(file_name, file_content):
-    pass
+    with open(f"{file_name}.txt", "w") as f:
+        f.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    with open(f"{file_name}.txt", "a") as f:
+        f.write(append_content)
+
+import os
 
 def read_file(file_name):
-    pass
+    if os.path.exists(f"{file_name}.txt"):
+        with open(f"{file_name}.txt", 'r') as f:
+            file_content = f.read()
+        return file_content
+    else:
+        return None
+
+
+
